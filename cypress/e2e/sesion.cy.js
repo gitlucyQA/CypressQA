ec2f7ae474e5da38c4ecae042a5a11c13316d74c
gloria morales <lucygit2960@gmail.com>
2024-11-18 13:02:14 -0500

    actualizacion final

--- cypress/e2e/sesion.cy.js
+++ cypress/e2e/sesion.cy.js
@@ -1,6 +1,31 @@
-it("inicio_sesion", () => {
-    cy.visit("https://www.google.es");
-    cy.get("#L2AGLb > .QS5gu").click();
-    cy.get("#APjFqb").type("Cypress{enter}");
-    cy.get('[href="https://www.cypress.io/"] > .LC20lb').click();
-  });
+
+describe ("test suite ecommerce",()=>{
+            
+    beforeEach("ingresar al home",()=>{
+      cy.visit("https://www.emcali.com.co/")
+      cy.url().should("contain", "emcali")
+    }) 
+      //inicio de sesión usuario no registrado
+      
+    
+       it("ingresar usuario",()=> {
+        cy.visit("https://www.emcali.com.co/login")
+        cy.url().should("contain","login")
+        cy.get("[name='_com_liferay_login_web_portlet_LoginPortlet_login']").type("lucygit2060@gmail.com")
+        cy.get("[name='_com_liferay_login_web_portlet_LoginPortlet_password']").type("password")
+              
+      })
+
+      //ingreso a una opcion del catalogo del submenu
+      it("Ingresar opcion atencion-al-usuario", ()=> {
+        cy.visit("https://www.emcali.com.co/es/web/guest/bc/-/knowledge_base/atencion-al-usuario/tramites")
+        cy.url().should("contain","atencion-al-usuario/tramites") 
+         })
+
+      //ingreso a una opciòn  de panel izquierdo de ocpcion de catalogo menu "atenciòn al usuario"
+        
+      it("Ingresar opcion centros de Atenciòn", ()=> {
+        cy.visit("https://www.emcali.com.co/es/bc/-/knowledge_base/atencion-al-usuario/centros-de-atencion")
+        cy.url().should("contain","centros-de-atencion")
+    })
+  })
